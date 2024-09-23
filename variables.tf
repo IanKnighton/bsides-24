9d92d3e85ff88f75d8f8f01279e3b47659982414
Ian Knighton <iknighton@deseretdigital.com>
2024-09-23 08:48:39 -0600

    chore: cleanup

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "default_project" {
-  description = "The default CCS Project"
-  default     = "ddm-platform"
-}
-
-variable "default_region" {
-  description = "Default CCS Region"
-  default     = "us-west3"
-}
